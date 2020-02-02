8bb345be35e1c073c5694844a9ede9a214ace621
League Student <league@iMac-23.attlocal.net>
2020-02-02 15:58:52 -0800

    pi aloud

--- src/_07_sleepy_head/SleepyHead.java
+++ src/_07_sleepy_head/SleepyHead.java
@@ -1,30 +1,44 @@
 package _07_sleepy_head;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
 
+		/* You MUST use the above boolean variables in your code */
 
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
-         */
-    }
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+		 * JOptionPane.YES_NO_OPTION);
+		 */
+		int days = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		if (days == JOptionPane.NO_OPTION) {
+			JOptionPane.showMessageDialog(null, "get up lazybones!");
+		} else {
+			if (days == JOptionPane.YES_OPTION) {
+				int vacay = JOptionPane.showConfirmDialog(null, "Are we on vacation?", "Sleepy Head",
+						JOptionPane.YES_NO_OPTION);
+				if (vacay == JOptionPane.YES_OPTION) {
+					JOptionPane.showMessageDialog(null, "sleep in");
+				} else {
+					if (vacay == JOptionPane.NO_OPTION) {
+						JOptionPane.showMessageDialog(null, "get up lazybones!");
+
+					}
+					/*
+					 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday, print
+					 * “get up lazybones!�? If it is a weekday, and we are on vacation, print “sleep
+					 * in�?.
+					 */
+				}
+			}
+		}
+	}
 }
--- src/testScores.java
+++ src/testScores.java
@@ -1,11 +1,31 @@
 import javax.swing.JOptionPane;
 
 public class testScores {
-public static void main(String[] args) {
-	String score = JOptionPane.showInputDialog(null, "What is your test score?");
-	int score2 = Integer.parseInt(score);
-if (score2 >= 90) {
-JOptionPane.showMessageDialog(null, "Wow! U R super smart!");
-}
-	}
+	public static void main(String[] args) {
+		String score = JOptionPane.showInputDialog(null, "What is your test score?");
+		double score2 = Double.parseDouble(score);
+		if (score2 >= 90) {
+			JOptionPane.showMessageDialog(null, "You could've done better");
+		} else {
+			if (score2 >= 80) {
+				JOptionPane.showMessageDialog(null, "I'm not mad, just disappointed");
+			} else {
+				if (score2 >= 70) {
+					JOptionPane.showMessageDialog(null, "NO DINNER FOR YOU YOUNG MAN!!");
+				} else {
+					if (score2 >= 60) {
+						JOptionPane.showMessageDialog(null, "Have fun sleeping with the rats outside!");
+					} else {
+						if (score2 >= 50) {
+							JOptionPane.showMessageDialog(null, "I am ashamed to even have you in this family");
+						} else {
+							if (score2 < 50) {
+								JOptionPane.showMessageDialog(null, "Wanna take a fun trip to the adoption center?!");
+							}
+						}
+					}
+				}
+			}
 		}
+	}
+}
