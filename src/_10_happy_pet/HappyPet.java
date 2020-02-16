d8cc58c68670abd6cf860f32faf19a433c746f14
League Student <league@iMac-109.attlocal.net>
2020-02-16 15:59:40 -0800

    ownAdventure

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -1,70 +1,82 @@
 package _10_happy_pet;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+
+	// 2. Add the following variable to the next line: static int happinessLevel =
+	// 0;
 	// this will be used to store the happiness of your pet
 	static int happinesslevel = 0;
+	static String pet = "";
+
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-String pet = JOptionPane.showInputDialog(null, "What kind of pet you want?");
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
+		pet = JOptionPane.showInputDialog(null, "What kind of pet you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		for (int i = 0; i < 4; i++) {
-			
+		for (int i = 0; i < 5; i++) {
+
+		
+		// 3. Use showOptionDialog to ask the user what they want to do to make their
+		// pet happy
+		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+		// Make sure to customize the title and question too.
+		int task = JOptionPane.showOptionDialog(null, "What will you do to make " + pet + " happy?",
+				"Insert Title Here", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Food", "Walk", "Clean up Poop", "Cuddle" }, null);
+
+		// 5. Use user input to call the appropriate method created in step 4.
+		if ((task) == 0) {
+			Food();
 		}
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "What will you do to make " + pet + " happy?", "Insert Title Here", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Food", "Walk", "Clean up Poop", "Cuddle" }, null);
-
-			// 5. Use user input to call the appropriate method created in step 4.
-if ((task)== 0){
-	Food();
-}
-if ((task)== 1){
-	Walk();
-}
-if ((task)== 2){
-	CleanupPoop();
-}
-if ((task)== 3){
-	Cuddle();
-}
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that they loves their pet and use break; to exit for loop.
-if (happinesslevel >= 50) {
-	JOptionPane.showMessageDialog(null, "Great news! You love " + pet + " , but i'm not sure if " + pet + " loves you.");
-}
+		if ((task) == 1) {
+			Walk();
+		}
+		if ((task) == 2) {
+			CleanupPoop();
+		}
+		if ((task) == 3) {
+			Cuddle();
+		}
+		// 6. If you determine the happiness level is large enough, tell the
+		// user that they loves their pet and use break; to exit for loop.
+		if (happinesslevel >= 50) {
+			JOptionPane.showMessageDialog(null,
+					"Great news! You love " + pet + " , but i'm not sure if " + pet + " loves you.");
+		break;
+		}
+		}
+		}
+		
 	
-	}
+
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
-static void Food() {
-happinesslevel -= 5;
-JOptionPane.showMessageDialog(null, "I think youre supposed to feed them animal food " );
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
+	static void Food() {
+		happinesslevel -= 5;
+		JOptionPane.showMessageDialog(null, "I think youre supposed to feed " + pet + " animal food ");
 
+	}
 
-}
-static void Walk() {
-happinesslevel -= 10;
-JOptionPane.showMessageDialog(null, "Maybe next time bring a leash" );
+	static void Walk() {
+		happinesslevel -= 10;
+		JOptionPane.showMessageDialog(null, "Maybe next time bring a leash");
 
+	}
 
-}
-static void CleanupPoop() {
-happinesslevel += 20;
-JOptionPane.showMessageDialog(null, "Teamwork makes the dream work!" );
+	static void CleanupPoop() {
+		happinesslevel -= 15;
+		JOptionPane.showMessageDialog(null, "Ewww! What did you feed " + pet);
 
+	}
 
-}
-static void Cuddle() {
-happinesslevel -= 15;
-JOptionPane.showMessageDialog(null, "" );
+	static void Cuddle() {
+		happinesslevel += 20;
+		JOptionPane.showMessageDialog(null, "That's very sweet");
 
-
-}
+	}
 
 }
---
+++ src/_11_own_adventure/ownAdventure.java
@@ -0,0 +1,83 @@
+package _11_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class ownAdventure {
+	public static void main(String[] args) {
+		int task = JOptionPane.showOptionDialog(null,
+				"The Joker and Riddler have escaped Arkham Asylum! What do you do?", "Batman and the Joker", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Send Robin", "Go Yourself", "Send Batgirl" },
+				null);
+
+		if ((task) == 0) {
+			Robin();
+		}
+		if ((task) == 1) {
+			Batman();
+		}
+		if ((task) == 2) {
+			Batgirl();
+		}
+
+	}
+
+	static void Robin() {
+		int saving = JOptionPane.showOptionDialog(null,
+				"Robin gets trapped by the Riddler and he is giving Batman one hour to come rescue him. Meanwhile, the Joker has rigged the Gotham City Bank to explode in one hour. Who do you go after?",
+				"Robin or Bank", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Go to Save Robin", "Go to Stop The Joker" }, null);
+		
+		if((saving)==0)
+
+		{
+			Saving();
+		}if((saving)==1)
+		{
+			Joker();
+		}
+	}
+static void Saving() {
+JOptionPane.showMessageDialog(null, "You take down the Riddler, free Robin, and call for backup. Then, you all rush over to the Bank, take out The Joker, and disarm the explosives with seconds to spare. The Bat-Family wins once again!  ");
+}
+static void Joker() {
+JOptionPane.showMessageDialog(null, "You choose to go after The Joker. You take him down and disarm the bombs with ten minutes to spare. But, the Riddler escapes once again after freeing Robin. You lost.");
+}
+	
+	
+	static void Batman() {
+		int hideout = JOptionPane.showOptionDialog(null,
+				"You manage to catch The Riddler and put him back in jail, but The Joker has run away! Where do you go to catch him?",
+				"Missing Joker", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Joker's Funhouse", "The Riddler's Hideout" }, null);
+		if((hideout)==0)
+
+		{
+			Funhouse();
+		}if((hideout)==1)
+		{
+			Hideout();
+		}
+	}
+	static void Funhouse() {
+		JOptionPane.showMessageDialog(null, "Your instinct tells you that Joker has fleed to his Funhouse. You make a surprise entrance into the Funhouse and find Joker making more explosives. You put him back into jail before he can run away. Batman has won once again!  ");
+		}
+		static void Hideout() {
+		JOptionPane.showMessageDialog(null, "Your instinct tells you that Joker has fleed to the Riddler's hideout to carry out their plan. After you make a surprise entrance, you are greeted with hundreds of explosives about to go off. You've been outsmarted this time. You lost.");
+		}
+static void Batgirl() {
+int backup = JOptionPane.showOptionDialog(null, "Batgirl catches The Joker and puts him in Arkham, but The Riddler frees The Penguin and they split up! Who does Batgirl call for backup?",
+			"Backup needed", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Nightwing and Robin",  "Batman" }, null);
+if((backup)==0)
+
+{
+	NightwingandRobin();
+}if((backup)==1)
+{
+	Batman();
+}
+
+}
+
+
+}
